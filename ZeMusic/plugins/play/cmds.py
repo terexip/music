a352f7d0ce27e5a98068515e30502dd330b2605a
botzon <2001har23@gmail.com>
2024-08-18 23:14:46 +0300

    Update cmds.py

--- ZeMusic/plugins/play/cmds.py
+++ ZeMusic/plugins/play/cmds.py
@@ -10,7 +10,7 @@ from ZeMusic import app
 from config import OWNER_ID, LOGGER_ID, START_IMG_URL
 import config
 
-@app.on_message(command(["ميوزك", "التشغيل "]))
+@app.on_message(command(["اوامر", "التشغيل "]))
 async def zdatsr(client: Client, message: Message):
     usr = await client.get_users(OWNER_ID)
     name = usr.first_name
@@ -22,12 +22,12 @@ async def zdatsr(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "🍻 اوامــر التشغيــل ", callback_data="zzzll"),
+                        " اوامــر التشغيــل ", callback_data="zzzll"),
                 ],[
                     InlineKeyboardButton(
-                        "🍻 اوامـر القنـاة ", callback_data="zzzch"),
+                        " اوامـر القنـاة ", callback_data="zzzch"),
                     InlineKeyboardButton(
-                        "🍻 اوامـر الادمـن ", callback_data="zzzad"),
+                        " اوامـر الادمـن ", callback_data="zzzad"),
                 ],[
                     InlineKeyboardButton(
                         text=config.CHANNEL_NAME, url=config.CHANNEL_LINK),
